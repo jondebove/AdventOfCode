ca501e65b76aba5a5b4d73a8042465143c29f1ad
Jonathan Debove <jondebove@outlook.com>
2025-09-08 18:02:34 +0200

    Rename Item in PriorityQueue to PriorityItem

--- 2021/graph.go
+++ 2021/graph.go
@@ -65,8 +65,9 @@ func BellmanFord[V comparable](g Grapher[V], s V) map[V]V {
 	prev := make(map[V]V, len(vs))
 	for n := len(vs) - 1; n > 0; n-- {
 		for _, u := range vs {
+			du := dist[u]
 			for _, v := range g.Neighbors(u) {
-				if d := dist[u] + g.Weight(u, v); d < dist[v] {
+				if d := du + g.Weight(u, v); d < dist[v] {
 					dist[v] = d
 					prev[v] = u
 				}
@@ -78,14 +79,14 @@ func BellmanFord[V comparable](g Grapher[V], s V) map[V]V {
 
 func Dijkstra[V comparable](g Grapher[V], s V) map[V]V {
 	vs := g.Vertices()
-	items := make(map[V]*Item, len(vs))
+	items := make(map[V]*PriorityItem, len(vs))
 	pq := make(PriorityQueue, len(vs))
 	for i, v := range vs {
 		d := math.Inf(1)
 		if v == s {
 			d = 0
 		}
-		item := Item{
+		item := PriorityItem{
 			value:    v,
 			priority: d,
 			index:    i,
@@ -97,14 +98,14 @@ func Dijkstra[V comparable](g Grapher[V], s V) map[V]V {
 
 	prev := make(map[V]V, len(vs))
 	for pq.Len() > 0 {
-		itemu := heap.Pop(&pq).(*Item)
-		u := itemu.value.(V)
+		pu := heap.Pop(&pq).(*PriorityItem)
+		u := pu.value.(V)
 		for _, v := range g.Neighbors(u) {
-			d := itemu.priority + g.Weight(u, v)
-			itemv := items[v]
-			if itemv.priority > d {
-				itemv.priority = d
-				heap.Fix(&pq, itemv.index)
+			d := pu.priority + g.Weight(u, v)
+			pv := items[v]
+			if pv.priority > d {
+				pv.priority = d
+				heap.Fix(&pq, pv.index)
 				prev[v] = u
 			}
 		}
@@ -119,16 +120,16 @@ func Dijkstra[V comparable](g Grapher[V], s V) map[V]V {
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// An Item is something we manage in a priority queue.
-type Item struct {
+// A PriorityItem is something we manage in a priority queue.
+type PriorityItem struct {
 	value    any     // The value of the item; arbitrary.
 	priority float64 // The priority of the item in the queue.
 	// The index is needed by update and is maintained by the heap.Interface methods.
 	index int // The index of the item in the heap.
 }
 
-// A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue []*Item
+// A PriorityQueue implements heap.Interface and holds PriorityItems.
+type PriorityQueue []*PriorityItem
 
 func (pq PriorityQueue) Len() int { return len(pq) }
 
@@ -144,7 +145,7 @@ func (pq PriorityQueue) Swap(i, j int) {
 
 func (pq *PriorityQueue) Push(x any) {
 	n := len(*pq)
-	item := x.(*Item)
+	item := x.(*PriorityItem)
 	item.index = n
 	*pq = append(*pq, item)
 }
